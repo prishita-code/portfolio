d69473a8ebbb838ff7f37424b19a89da3b631b15
prishita-code <prishitasingh8@gmail.com>
2025-09-08 22:14:22 +0100

    Commit

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'isomorphic-fetch';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
